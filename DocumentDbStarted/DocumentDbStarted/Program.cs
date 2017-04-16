07d40aa6f21dc11d485d0250f6fefac1ab28c751
deepgautam <deepgautam2008@gmail.com>
2017-04-16 16:22:07 +0530

    C# hands on by Deep Gautam

---
+++ DocumentDbStarted/DocumentDbStarted/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Net;
+using Microsoft.Azure.Documents;
+using Microsoft.Azure.Documents.Client;
+using Newtonsoft.Json;
+
+namespace DocumentDbStarted
+{
+    class Program
+    {
+        // ADD THIS PART TO YOUR CODE
+        private const string EndpointUri = "https://deep.documents.azure.com:443/";
+        private const string PrimaryKey = "lBMrGoeAMMZMxJy3cEvvwGcpbz3QlTBjRcBdHaYslOO9pQPL2R8aqESRvifRT9cQzKNF2ZEQMSD6nMN9u6orAQ==";
+        private DocumentClient client;
+
+        static void Main(string[] args)
+        {
+            // ADD THIS PART TO YOUR CODE
+            try
+            {
+                Program p = new Program();
+                p.GetStartedDemo().Wait();
+            }
+            catch (DocumentClientException de)
+            {
+                Exception baseException = de.GetBaseException();
+                Console.WriteLine("{0} error occurred: {1}, Message: {2}", de.StatusCode, de.Message, baseException.Message);
+            }
+            catch (Exception e)
+            {
+                Exception baseException = e.GetBaseException();
+                Console.WriteLine("Error: {0}, Message: {1}", e.Message, baseException.Message);
+            }
+            finally
+            {
+                Console.WriteLine("End of demo, press any key to exit.");
+                Console.ReadKey();
+            }
+        }
+
+        // ADD THIS PART TO YOUR CODE
+        private async Task GetStartedDemo()
+        {
+            this.client = new DocumentClient(new Uri(EndpointUri), PrimaryKey);
+            // ADD THIS PART TO YOUR CODE
+            await this.CreateDatabaseIfNotExists("FamilyDB_oa");
+        }
+        // ADD THIS PART TO YOUR CODE
+        private void WriteToConsoleAndPromptToContinue(string format, params object[] args)
+        {
+            Console.WriteLine(format, args);
+            Console.WriteLine("Press any key to continue ...");
+            Console.ReadKey();
+        }
+        // ADD THIS PART TO YOUR CODE
+        private async Task CreateDatabaseIfNotExists(string databaseName)
+        {
+            // Check to verify a database with the id=FamilyDB does not exist
+            try
+            {
+                await this.client.ReadDatabaseAsync(UriFactory.CreateDatabaseUri(databaseName));
+                this.WriteToConsoleAndPromptToContinue("Found {0}", databaseName);
+            }
+            catch (DocumentClientException de)
+            {
+                // If the database does not exist, create a new database
+                if (de.StatusCode == HttpStatusCode.NotFound)
+                {
+                    await this.client.CreateDatabaseAsync(new Database { Id = databaseName });
+                    this.WriteToConsoleAndPromptToContinue("Created {0}", databaseName);
+                }
+                else
+                {
+                    throw;
+                }
+            }
+        }
+
+    }
+}
---
+++ OOPSPractice/OOPSPractice/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPSPractice
+{
+    class Animal
+    {
+        public double Hieght { get; set; }
+        public double  Weight { get; set; }
+        public string Name { get; set; }
+        public string Sound { get; set; }
+        public Animal()
+        {
+            this.Hieght = 0;
+            this.Weight = 0;
+            this.Name = "No Name";
+            this.Sound = "No sound";
+            numOfAnimals++;
+        }
+        public Animal(double hieght,double wieght,string name,string sound)
+        {
+            this.Hieght = hieght;
+            this.Weight = wieght;
+            this.Name = name;
+            this.Sound = sound;
+            numOfAnimals++;  
+        }
+        static int numOfAnimals = 0;
+        public static int GetNumofAnimals()
+        {
+            return numOfAnimals;
+        }
+        public string toString()
+        {
+            return string.Format("{0} is {1} inches tall, weighs {2} lbs and like to say {3} ",Name,Hieght,Weight,Sound);
+        }
+        static void Main(string[] args)
+        {
+            Animal spot = new Animal(15, 10, "spot", "woof");
+            Console.WriteLine("{0} says {1}",spot.Name,spot.Sound);
+            Console.WriteLine("No of animals is  "+ Animal.GetNumofAnimals());
+            Console.WriteLine(spot.toString());
+
+            Console.ReadLine();
+
+        }
+    }
+}
